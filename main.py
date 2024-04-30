18ace161fa7731530a3a35d80c4db5e159635844
missnichka <166759482+missnichka@users.noreply.github.com>
2024-04-30 18:22:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+x = 38
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print('дратути!')
+if x < 0:
+    print('Меньше нуля')
+print('дотвидания!')
+# примеры
+a, b = 10, 5
 
+if a > b:
+    print('a > b')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if a > b and a > 0:
+    print('успех')
 
+if(a > b) and (a > 0 or b < 1000):
+    print('успех')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if 5 < b and b < 10:
+    print('успех')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# можно сравнивать - числа, строки, списки и тд.
+
+if '34' > '123':
+    print('успех')
+
+if '123' > '12':
+    print('успех')
+
+if [1, 2] > [1, 1]:
+    print('успех')
+
+# нельзя сравнивать разные типы данных
+
+if '6' > 5:
+    print('успех')
+
+if [5, 6] > 5:
+    print('успех')
+
+# но
+
+if '6' != 5:
+    print('успех')
